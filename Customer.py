ec4b71875a4246c6d4f4315683c4de14f80bb578
JiaxuanFelixLi <z5086369@ad.unsw.edu.au>
2019-09-19 13:01:03 +1000

    Assignment1

--- Customer.py
+++ Customer.py
@@ -11,6 +11,10 @@ class Customer(object):
     def __str__(self):
         return "Customer Name {0}, Customer Age {1}, Driving license {2}".format(self._name, self._age, self._license)
 
+# main function
 if __name__ == '__main__':
+    # instance : customer
     customer = Customer('felix', '24', 'NB123123')
+    customer._name = 'david'
     print(customer.__str__())
+    #assert(customer._name == 'felix')
